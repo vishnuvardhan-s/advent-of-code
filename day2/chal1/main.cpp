73ac854b228e88be404f3d69149c5b4b5348ecff
Vishnuvardhan S <greenmatrix22@gmail.com>
2024-12-02 11:37:05 +0530

    day2 chal1, chal2

---
+++ day2/chal1/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int INCREASING = 1;
+int DECREASING = -1;
+
+vector<vector<int>> get_input()
+{
+    int num;
+    vector<vector<int>> data;
+    while (cin >> num)
+    {
+        vector<int> row;
+        row.push_back(num);
+        while (cin.peek() == ' ' || cin.peek() == '\t')
+        {
+            cin >> num;
+            row.push_back(num);
+        }
+        data.push_back(row);
+        cin.ignore();
+    }
+    return data;
+}
+
+bool ok(vector<int> &a, int flow)
+{
+    int prev = a[0];
+    for (int i = 1; i < a.size(); i++)
+    {
+        int diff = flow == INCREASING ? a[i] - prev : prev - a[i];
+        if (diff < 1 or diff > 3)
+        {
+            return false;
+        }
+        prev = a[i];
+    }
+    return true;
+}
+
+int main()
+{
+    vector<vector<int>> data = get_input();
+    int ans = 0;
+    for (int i = 0; i < data.size(); i++)
+    {
+        if (ok(data[i], INCREASING))
+        {
+            ans++;
+        }
+        else if (ok(data[i], DECREASING))
+        {
+            ans++;
+        }
+    }
+    cout << "Safe Reports: " << ans;
+}
